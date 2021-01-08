3a02d04cc8eb64f2b8de76ee5722fae985d617fc
Jejo <jejomardorongon@gmail.com>
2021-01-08 08:56:48 +0800

    Removed referrer

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,18 +5,13 @@
  * See: https://www.gatsbyjs.com/docs/use-static-query/
  */
 
-import React, { useEffect } from "react"
+import React from "react"
 import PropTypes from "prop-types"
 
 import Header from "./header"
 import Footer from "./Footer"
 
 const Layout = ({ children }) => {
-  useEffect(() => {
-    console.log(document.referrer)
-    console.log(window.location.search)
-  }, [])
-
   return (
     <div className="flex flex-col min-h-screen">
       <Header />

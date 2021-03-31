76731a711ac3139a7a689ed944a7ee1715bc0239
Jejo <jejomardorongon@gmail.com>
2021-03-31 09:37:47 +0800

    Finished home hero animations

--- src/components/Home/AnimatedSVGSlider.js
+++ src/components/Home/AnimatedSVGSlider.js
@@ -2,11 +2,12 @@ import React, { useEffect } from "react"
 import { useCycle, AnimatePresence } from "framer-motion"
 import AnimatedSVG from "./AnimatedSVG"
 import AnimatedSVG2 from "./AnimatedSVG2"
+import AnimatedSVG3 from "./AnimatedSVG3"
 
 const AnimatedSVGSlider = () => {
-  const [cycle, setCycle] = useCycle(0, 1)
+  const [cycle, setCycle] = useCycle(0, 1, 2)
 
-  const svgs = [<AnimatedSVG />, <AnimatedSVG2 />]
+  const svgs = [<AnimatedSVG />, <AnimatedSVG2 />, <AnimatedSVG3 />]
 
   useEffect(() => {
     const slideshowTimer = setTimeout(() => {

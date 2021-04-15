d0f385e8125fa11f7b20d01dbc8a5078ac78aa38
Jejo <jejomardorongon@gmail.com>
2021-04-15 08:27:34 +0800

    Finished hero animation

--- src/components/Home/AnimatedSVGSlider.js
+++ src/components/Home/AnimatedSVGSlider.js
@@ -2,16 +2,17 @@ import React, { useEffect } from "react"
 import { useCycle, AnimatePresence } from "framer-motion"
 import ReAnimatedSvg1 from "./ReAnimatedSvg1"
 import ReAnimatedSvg2 from "./ReAnimatedSvg2"
+import ReAnimatedSVG3 from "./ReAnimatedSVG3"
 
 const AnimatedSVGSlider = () => {
-  const [cycle, setCycle] = useCycle(0, 1)
+  const [cycle, setCycle] = useCycle(0, 1, 2)
 
-  const svgs = [<ReAnimatedSvg1 />, <ReAnimatedSvg2 />]
+  const svgs = [<ReAnimatedSvg1 />, <ReAnimatedSvg2 />, <ReAnimatedSVG3 />]
 
   useEffect(() => {
     const slideshowTimer = setTimeout(() => {
       setCycle()
-    }, 6000)
+    }, 7000)
     return () => clearTimeout(slideshowTimer)
   })
 
--- src/components/Home/HomeHero.js
+++ src/components/Home/HomeHero.js
@@ -5,9 +5,6 @@ import HeroWave from "../../assets/herowave.svg"
 import { graphql, useStaticQuery } from "gatsby"
 import { GatsbyImage, getImage } from "gatsby-plugin-image"
 import AnimatedSVGSlider from "./AnimatedSVGSlider"
-import ReAnimatedSvg1 from "./ReAnimatedSvg1"
-import ReAnimatedSvg2 from "./ReAnimatedSvg2"
-import ReAnimatedSVG3 from "./ReAnimatedSVG3"
 
 const query = graphql`
   query {
@@ -63,10 +60,7 @@ const HomeHero = ({ heroFields }) => {
         </div>
 
         <div className="hero-svg">
-          {/* <AnimatedSVGSlider /> */}
-          {/* <ReAnimatedSvg1 /> */}
-          {/* <ReAnimatedSvg2 /> */}
-          <ReAnimatedSVG3 />
+          <AnimatedSVGSlider />
         </div>
       </div>
 

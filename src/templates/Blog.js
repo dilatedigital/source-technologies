6c2bbb403e126a479810c8343c0673286607cc72
Jejo <jejomardorongon@gmail.com>
2021-04-30 16:49:45 +0800

    Fixed blog hero image quality

--- src/templates/Blog.js
+++ src/templates/Blog.js
@@ -129,7 +129,11 @@ export const query = graphql`
         heroImage {
           localFile {
             childImageSharp {
-              gatsbyImageData(layout: FULL_WIDTH, placeholder: TRACED_SVG)
+              gatsbyImageData(
+                layout: FULL_WIDTH
+                placeholder: TRACED_SVG
+                quality: 100
+              )
             }
           }
         }

e9d5c1168c246ace045ac67f29bb795a06359573
Dilate - Jejo <developers@dilate.com.au>
2021-06-09 15:48:18 +0800

    Added gtag

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,14 +17,28 @@ module.exports = {
   },
   plugins: [
     {
-      resolve: `gatsby-plugin-google-analytics`,
+      resolve: `gatsby-plugin-google-gtag`,
       options: {
-        trackingId: "G-G18PEBW8EK",
-        // this option places the tracking script into the head of the DOM
-        head: true,
-        // other options
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-G18PEBW8EK", // Google Analytics / GA
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        gtagConfig: {
+          optimize_id: "OPT_CONTAINER_ID",
+          anonymize_ip: true,
+          cookie_expires: 0,
+        },
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: true,
+          // Setting this parameter is also optional
+        },
       },
     },
+
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,

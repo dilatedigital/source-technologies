7aa825921488caca244ba7f7f95873f397359885
Dilate - Jejo <developers@dilate.com.au>
2021-06-10 10:24:24 +0800

    Changed GA id

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,7 +21,7 @@ module.exports = {
       options: {
         // You can add multiple tracking ids and a pageview event will be fired for all of them.
         trackingIds: [
-          "G-G18PEBW8EK", // Google Analytics / GA
+          "G-JVYZT6NQG3", // Google Analytics / GA
         ],
         // This object gets passed directly to the gtag config command
         // This config will be shared across all trackingIds
